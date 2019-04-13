436b85633ca3e7034e5b31dc7d61c00752415af8
MikeVerba <michal.wierba@gmail.com>
2019-04-13 15:12:24 +0200

    added hardcoded security configuration

---
+++ src/main/java/com/example/travelagencyapi/config/CustomWebSecurityAdapter.java
@@ -0,0 +1,49 @@
+package com.example.travelagencyapi.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class CustomWebSecurityAdapter extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private MyBasicAuthenticationEntryPoint authenticationEntryPoint;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
+        authenticationManagerBuilder.inMemoryAuthentication()
+                .withUser("user1")
+                .password(passwordEncoder().encode("user1Pass"))
+                .authorities("ROLE_USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.authorizeRequests()
+                .antMatchers("securityNone").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .authenticationEntryPoint(authenticationEntryPoint);
+
+        httpSecurity.addFilterAfter(new CustomFilter() ,BasicAuthenticationFilter.class);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+
+
+}
---
+++ src/main/java/com/example/travelagencyapi/config/MyBasicAuthenticationEntryPoint.java
@@ -0,0 +1,30 @@
+package com.example.travelagencyapi.config;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Component
+public class MyBasicAuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+
+        httpServletResponse.addHeader("WWW-Authenticate","Basic realm=" + getRealmName() + "");
+        httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        PrintWriter writer = httpServletResponse.getWriter();
+        writer.println("HTTP Status 401 - " + e.getMessage());
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception{
+        setRealmName("MikeVerba");
+        super.afterPropertiesSet();
+    }
+}
