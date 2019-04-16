096212b801e8e7a59071bb4e45cfa58471878d65
MikeVerba <michal.wierba@gmail.com>
2019-04-16 06:38:46 +0200

    preparing for deployment on AWS

--- src/main/java/com/example/travelagencyapi/config/CustomWebSecurityAdapter.java
+++ src/main/java/com/example/travelagencyapi/config/CustomWebSecurityAdapter.java
@@ -29,7 +29,7 @@ public class CustomWebSecurityAdapter extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception{
         httpSecurity.authorizeRequests()
-                .antMatchers("securityNone").permitAll()
+                .antMatchers("/securityNone").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic()
