a022f85a6f38c18097680ff8d22c93123296276f
MikeVerba <michal.wierba@gmail.com>
2019-08-05 07:46:33 +0200

    expanded test for rest controllers

--- src/test/java/com/example/travelagencyapi/controllers/ClientControllerTest.java
+++ src/test/java/com/example/travelagencyapi/controllers/ClientControllerTest.java
@@ -3,6 +3,7 @@ package com.example.travelagencyapi.controllers;
 import com.example.travelagencyapi.api.models.ClientDto;
 import com.example.travelagencyapi.services.ClientServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -16,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.BDDMockito.*;
@@ -53,23 +55,35 @@ class ClientControllerTest {
     }
 
     @Test
+    @DisplayName("Testing getting all clients - no parameters")
     void getAllClients() throws Exception {
 
         given(clientService.getAllClients()).willReturn(clientDtoList);
 
-        mockMvc.perform(get("/api/v1/clients/all"))
+        mockMvc.perform(get("/api/v1/clients/all").accept(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(status().isOk())
+                .andExpect(jsonPath("$",hasSize(1)))
+                .andExpect(jsonPath("$[0].id",is(1)))
+                .andExpect(jsonPath("$[0].firstname",is("Jan")))
+                .andExpect(jsonPath("$[0].lastname",is("Pazyl")))
+                .andExpect(jsonPath("$[0].client_url",is("someurl")))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+
+        //when working against real database the order of clients can be different!
     }
 
     @Test
+    @DisplayName("Testing getting single client")
     void getClientById() throws Exception {
         given(clientService.getClientById(anyLong())).willReturn(clientDto);
 
-        mockMvc.perform(get("/api/v1/clients/1"))
+        mockMvc.perform(get("/api/v1/clients/" + clientDto.getId()))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-                .andExpect(jsonPath("$.id",is(1)));
+                .andExpect(jsonPath("$.id",is(1)))
+                .andExpect(jsonPath("$.lastname", is("Pazyl")))
+                .andExpect(jsonPath("$.firstname", is("Jan")));
+
     }
 
     @Test
--- src/test/java/com/example/travelagencyapi/controllers/OfferControllerTest.java
+++ src/test/java/com/example/travelagencyapi/controllers/OfferControllerTest.java
@@ -7,6 +7,8 @@ import com.example.travelagencyapi.domain.Continent;
 import com.example.travelagencyapi.domain.Offer;
 import com.example.travelagencyapi.services.OfferServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -20,12 +22,17 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 class OfferControllerTest {
@@ -94,40 +101,79 @@ class OfferControllerTest {
     }
 
     @Test
+    @DisplayName("Testing getting all offers  - no parameters")
     void getAllOffers() throws Exception{
 
         given(offerService.getAllOffers()).willReturn(offerDtoList);
 
-        mockMvc.perform(get("/api/v1/offers/all"))
+        mockMvc.perform(get("/api/v1/offers/all").accept(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(status().isOk())
+                .andExpect(jsonPath("$",hasSize(1)))
+                .andExpect(jsonPath("$[0].id",is(1)))
+                .andExpect(jsonPath("$[0].continent",is(Continent.AF.toString())))
+                .andExpect(jsonPath("$[0].pricePerNight",is(12.99)))
+                .andExpect(jsonPath("$[0].client.id",is(1)))
+                .andExpect(jsonPath("$[0].client.firstname",is("Jan")))
+                .andExpect(jsonPath("$[0].client.lastname",is("Pazyl")))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+
+        //when working against real database the order of offers can be different!
     }
 
     @Test
+    @DisplayName("Testing getting all Unbooked Offers")
     void getAllUnBookedOffers() throws Exception {
         given(offerService.getAllUnbookedOffers()).willReturn(offerDtoList);
 
         mockMvc.perform(get("/api/v1/offers/unbooked"))
+                .andExpect(jsonPath("$",hasSize(1)))
+                .andExpect(jsonPath("$[0].id",is(1)))
+                .andExpect(jsonPath("$[0].continent",is(Continent.AF.toString())))
+                .andExpect(jsonPath("$[0].pricePerNight",is(12.99)))
+                .andExpect(jsonPath("$[0].client.id",is(1)))
+                .andExpect(jsonPath("$[0].client.firstname",is("Jan")))
+                .andExpect(jsonPath("$[0].client.lastname",is("Pazyl")))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
     }
 
-//    @Test
-//    void getAllOffersForClient() throws Exception{
-//
-//        given(offerService.getAllBookedOffersForClient(any())).willReturn(offerDtoList);
-//
-//        mockMvc.perform(get("/api/v1/offers/booked-for-client"))
-//                .andExpect(status().isOk())
-//                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
-//    }//todo learn how to send json with mockmvc
+    @Test
+    @Disabled
+    void getAllOffersForClient() throws Exception{
+
+        given(offerService.getAllBookedOffersForClient(any())).willReturn(offerDtoList);
+
+        mockMvc.perform(get("/api/v1/offers/booked-for-client"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+    }//throws 400 error
 
     @Test
-    void getAllOffersQualifiedByConditions() {
+    void getAllOffersQualifiedByConditions() throws Exception {
+
+        given(offerService.getAllOffersQualifiedByConditions(any())).willReturn(offerDtoList);
+
+        mockMvc.perform(get("/api/v1/offers/booked-for-conditions").accept(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk());
+
+
     }
 
     @Test
-    void getOfferById() {
+    @DisplayName("Testing getting single offer")
+    void getOfferById() throws Exception{
+
+        given(offerService.getOfferById(anyLong())).willReturn(offerDto);
+
+        mockMvc.perform(get("/api/v1/offers/"+offerDto.getId().toString()))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.id",is(1)))
+                .andExpect(jsonPath("$.offer_url", is("someurl")))
+                .andExpect(jsonPath("$.numberOfNights", is(4)))
+                .andExpect(jsonPath("$.continent", is(Continent.AF.toString())))
+                .andExpect(jsonPath("$.dogAllowed", is(true)))
+                .andExpect(jsonPath("$.pricePerNight",is(12.99)));
     }
 
     @Test
@@ -143,6 +189,12 @@ class OfferControllerTest {
     }
 
     @Test
-    void deleteOffer() {
+    void deleteOffer() throws Exception {
+
+        mockMvc.perform(delete("/api/v1/offers/"+offer.getId()))
+                .andExpect(status().isOk());
+
+        then(offerService).should().deleteOffer(offer.getId());
+
     }
 }
