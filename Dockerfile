e1e5262b0070e99fbf64255d524ee980e887e800
MikeVerba <mikeverba@MacBook-Pro.local>
2023-02-06 14:15:51 +0100

    Fix long method, fix dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,3 @@
-FROM openjdk:8
-
-#RUN "mvn clean install"
-#
-## Refer to Maven build -> finalName
-#ARG JAR_FILE=target/travel-agency-api.jar
-#
-## cd /opt/app
-#WORKDIR /opt/app
-#
-## cp target/spring-boot-web.jar /opt/app/app.jar
-#COPY ${JAR_FILE} app.jar
-#
-## java -jar /opt/app/app.jar
-#ENTRYPOINT ["java","-jar","app.jar"]
-
-
 #
 # Build stage
 #
--- src/test/java/com/example/travelagencyapi/controllers/BookingControllerTest.java
+++ src/test/java/com/example/travelagencyapi/controllers/BookingControllerTest.java
@@ -13,13 +13,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -27,33 +25,49 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ExtendWith(MockitoExtension.class)
 class BookingControllerTest {
-
     @Mock
     BookingServiceImpl bookingService;
-
     @InjectMocks
     BookingController bookingController;
-
     MockMvc mockMvc;
-
     List<Offer> offerList;
-
     Offer offer;
-
     OfferDto offerDto;
-
     Client client;
-
-    List<Client>clientList;
-
+    List<Client> clientList;
     ClientDto clientDto;
 
     @BeforeEach
     void setUp() {
+        offerList = createOfferList();
+        client = createClient();
+        offerDto = createOfferDto();
+        mockMvc = MockMvcBuilders.standaloneSetup(bookingController).build();
+    }
 
-        offerList = new ArrayList<>();
+    private OfferDto createOfferDto() {
+        OfferDto offerDto = new OfferDto();
+        offerDto.setId(1L);
+        offerDto.setOfferUrl("someurl");
+        offerDto.setClient(client);
+        offerDto.setContinent(Continent.AF);
+        offerDto.setNumberOfNights(4);
+        offerDto.setPricePerNight(12.99F);
+        offerDto.setDogAllowed(true);
+        return offerDto;
+    }
 
+    private Client createClient() {
+        Client client = new Client();
+        client.setId(1L);
+        client.setFirstname("Jan");
+        client.setLastname("Pazyl");
+        client.setBookedOffers(offerList);
+        return client;
+    }
 
+    private List<Offer> createOfferList() {
+        List<Offer> offerList = new ArrayList<>();
         offer = new Offer();
         offer.setId(1L);
         offer.setClient(client);
@@ -62,33 +76,16 @@ class BookingControllerTest {
         offer.setIsOfferBooked(false);
         offer.setPricePerNight(12.99F);
         offer.setNumberOfNights(4);
-
-        client = new Client();
-        client.setId(1L);
-        client.setFirstname("Jan");
-        client.setLastname("Pazyl");
-        client.setBookedOffers(offerList);
-
-        offerDto = new OfferDto();
-        offerDto.setId(1L);
-        offerDto.setOfferUrl("someurl");
-        offerDto.setClient(client);
-        offerDto.setContinent(Continent.AF);
-        offerDto.setNumberOfNights(4);
-        offerDto.setPricePerNight(12.99F);
-        offerDto.setDogAllowed(true);
-
-        mockMvc = MockMvcBuilders.standaloneSetup(bookingController).build();
+        return offerList;
     }
 
     @Test
     void bookOfferForClientId() throws Exception {
-
-        given(bookingService.bookOfferForClientId(anyLong(),anyLong())).willReturn(offerDto);
+        given(bookingService.bookOfferForClientId(anyLong(), anyLong())).willReturn(offerDto);
 
         mockMvc.perform(post("/api/v1/booking/1/1/")
-        .param("client_id","1")
-        .param("offer_id","1"))
+                        .param("client_id", "1")
+                        .param("offer_id", "1"))
                 .andExpect(status().isOk());
     }
 }
